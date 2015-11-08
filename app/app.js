595b698cef23b08e08e72ffa9c8c8d72fc729bf4
Vivek Bharatha <vivek.bharatha@gmail.com>
2015-11-08 17:33:15 +0530

    Added bootstrap and logs listing view done:)

--- app/app.js
+++ app/app.js
@@ -2,4 +2,13 @@
  * Created by v3xvard on 8/11/15.
  */
 
-var app = angular.module("myLog", []);
+var app = angular.module("myLog", ['ngRoute', 'ngMaterial', 'ngAnimate', 'ngAria']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: "app/views/list.html",
+        controller: "logsListController"
+    }).otherwise({
+        redirectTo: '/'
+    });
+}]);
--- app/controllers/logsListController.js
+++ app/controllers/logsListController.js
@@ -2,9 +2,9 @@
  * Created by v3xvard on 8/11/15.
  */
 
-app.controller("logsListController", function ($scope) {
+app.controller("logsListController", ["$scope", function ($scope) {
     $scope.logs = [
-        {title: 'one', content: 'heheheh conternt', date: '27-10-2015'},
-        {title: 'two', content: 'heheheh conternt', date: '28-10-2015'}
+        {title: 'one', content: 'one content', date: '27-10-2015'},
+        {title: 'two', content: 'two content', date: '28-10-2015'}
     ]
-});
+}]);
