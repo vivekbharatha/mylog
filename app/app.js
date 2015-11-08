5fe09652158f68b3c839fb10afaf9c495532cab8
Vivek Bharatha <vivek.bharatha@gmail.com>
2015-11-08 15:58:44 +0530

    Added basic app structure

---
+++ app/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by v3xvard on 8/11/15.
+ */
+
+var app = angular.module("myLog", []);
---
+++ app/controllers/logsListController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by v3xvard on 8/11/15.
+ */
+
+app.controller("logsListController", function ($scope) {
+    $scope.logs = [
+        {title: 'one', content: 'heheheh conternt', date: '27-10-2015'},
+        {title: 'two', content: 'heheheh conternt', date: '28-10-2015'}
+    ]
+});
